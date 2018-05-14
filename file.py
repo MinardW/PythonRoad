f62c3b1120f1d7c5b44a1aec6262234d88524322
Minard_Wu <Minard_Wu@asus.com>
2018-05-14 11:49:19 +0800

    Update file function
    Change-Id: Ifbdf727b4dccb30ca938afe86010122fdba44ffa

--- file.py
+++ file.py
@@ -1,9 +1,35 @@
 import os
+import sys
 
-path = 'C:\\DriveKey\\EULA.doc'
+#输入一个文件的具体路径
+#输入一个需要查找该根目录下的文件类型(.txt/.py/....)
+
+def help():
+	print('Function Dec:Find the file of the specified type.')
+	print('*1.Input the absolute path of a file.')
+	print('*2.Input the file type to be searched for.\n')
+	print('Remark:If you not input second para,default get file type of first para.')
+
+#path = 'C:\\DriveKey\\EULA.doc'
 #P143
 #列出一个文件的根目录下所有文件,并得到文件容量
-for p in os.listdir(os.path.dirname(path)):
-	print(os.path.join(os.path.dirname(path),p),end="")
-	print( (' Size:%d byte' % os.path.getsize(os.path.join(os.path.dirname(path),p))).center(30))
+if(len(sys.argv) < 2):
+	print("invaild input.")
+	help()
+	sys.exit()
+	
+for p in os.listdir(os.path.dirname(sys.argv[1])):
+	fileType = ''
+
+	if(len(sys.argv) == 2):
+		fileType = '.'+os.path.basename(sys.argv[1]).split('.')[1]
+	else:
+		fileType = sys.argv[2]
+		
+	if(fileType in p):
+		filePath = os.path.join(os.path.dirname(sys.argv[1]),p)
+		print(filePath)
+	
+	#print(os.path.join(os.path.dirname(path),p),end="")
+	#print( (' Size:%d byte' % os.path.getsize(os.path.join(os.path.dirname(path),p))).center(30))
 	
